17bd1c5a66abab037bd8593f2ed5c80d8b8d88f8
giacomo <giacomo@mail.com>
2020-01-08 09:35:20 +0100

    Override back button in the second activity

--- app/src/main/java/it/adfm2019/testnavigation/SubActivityPage.java
+++ app/src/main/java/it/adfm2019/testnavigation/SubActivityPage.java
@@ -1,13 +1,24 @@
 package it.adfm2019.testnavigation;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager2.widget.ViewPager2;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class SubActivityPage extends AppCompatActivity {
     ViewPager2 pager;
+
+    @Override
+    public void onBackPressed() {
+        int currentPage = pager.getCurrentItem();
+
+        if(currentPage > 0 )
+            pager.setCurrentItem(currentPage-1);
+        else
+            super.onBackPressed();
+
+        return;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
